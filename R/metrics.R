680ca914c85552f277336e87b53cb7a6ee04a444
jsta <stachel2@msu.edu>
2018-01-08 10:13:58 -0500

    list some metrics

---
+++ R/metrics.R
@@ -0,0 +1,11 @@
+# fetch - longest un-interupted straight-line distance
+
+# perimeter length
+
+# covex hull
+
+# minor-major axis length/ratio
+
+# dispersion - average distance from all points on the perimeter to centroid
+
+# girth index - the radius of the largest inscribed circle
